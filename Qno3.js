26d2eecefaf3e9d29898dfffeb89ab726b774cb1
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:23:23 +0500

    Qno03 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno03 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno3.js
@@ -0,0 +1,7 @@
+"use strict";
+/*3. Name Cases: Store a person’s name in a variable, and then print that person’s name in
+ lowercase, uppercase, and titlecase.*/
+let firstName = "khanzadi wazirali";
+console.log(firstName.toLowerCase()); //lowercase
+console.log(firstName.toUpperCase()); //uppercase
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1)); //titlecase
